e0a88e2b48d62df17c8a18f50dc100b9476e5371
Owen Adams <owenadams.main@gmail.com>
2022-03-25 18:31:46 -0400

    update styling add div

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="container">
-      <header className="sub-container">
-        <h1>Smith Counseling Centre</h1>
+      <header className="row nav-bar">
+        <div className="col-half">
+          <h1>Smith Counseling Centre</h1>
+        </div>
+        <div className="col-half">
+
+        </div>
       </header>
     </div>
   );
