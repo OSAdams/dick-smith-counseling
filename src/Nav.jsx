728743b6e80954c8cab1cccc222081dbaab3c443
Owen Adams <owenadams.main@gmail.com>
2022-03-27 17:15:16 -0400

    add function to nav-links, work in progress

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -6,17 +6,25 @@ export default class Nav extends React.Component {
     super(props);
     this.state = { navigation: null }
   }
+
+  isActive() {
+    const navLinks = document.querySelectorAll('.nav-link')
+    for (let navLinkIndex = 0; navLinkIndex < navLinks.length; navLinkIndex++) {
+
+    }
+  }
+
   render() {
     return (
           <header className="row nav-bar flex">
             <div className="col-half nav-title">
               <h1>Smith Counseling Centre</h1>
             </div>
-            <div className="col-half nav-links">
-              <a className= "nav-link" href="google.com">Home</a>
-              <a className= "nav-link" href="google.com">Information</a>
-              <a className= "nav-link" href="google.com">Contact</a>
-              <a className= "nav-link" href="google.com">Inquire</a>
+            <div className="col-half nav-link-list">
+              <a className="nav-link" href="google.com">Home</a>
+              <a className="nav-link" href="google.com">Information</a>
+              <a className="nav-link" href="google.com">Contact</a>
+              <a className="nav-link" href="google.com">Inquire</a>
             </div>
           </header>
     );
